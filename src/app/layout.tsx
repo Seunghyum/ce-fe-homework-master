9e2620e860a7fee61c3164aec9ba7604b279ce95
Seunghyum <antmgu@gmail.com>
2025-07-01 10:56:44 +0900

    feat: add HeaderGNB

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
+import HeaderGNB from "@/layout/HeaderGNB";
 
 const notoSansKR = Noto_Sans_KR({
   subsets: ["latin"],
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${notoSansKR.className} antialiased`}>{children}</body>
+      <body className={`${notoSansKR.className} antialiased`}>
+        <HeaderGNB />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/layout/HeaderGNB.tsx
@@ -0,0 +1,12 @@
+export default function HeaderGNB() {
+  return (
+    <header className="border-b-2 border-gray-200 p-4 flex justify-between items-center">
+      <h1 className="text-2xl">Dashboard</h1>
+      <nav>
+        <ul className="flex gap-4">
+          <li>settings</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
