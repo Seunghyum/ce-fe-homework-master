3b6a332b83856e81b024fce2ee5bc0f30316a5e3
Seunghyum <antmgu@gmail.com>
2025-07-01 11:17:55 +0900

    feat: add not-found page

--- src/layout/SidebarLNB.tsx
+++ src/layout/SidebarLNB.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
+
 export default function SidebarLNB() {
   return (
     <div className="p-4 w-64">
       <nav>
         <ul className="flex flex-col gap-2">
-          <li>홈</li>
-          <li>서비스게시판</li>
+          <Link href="/">
+            <li>홈</li>
+          </Link>
+          <Link href="/service-board">
+            <li>서비스게시판</li>
+          </Link>
         </ul>
       </nav>
     </div>
