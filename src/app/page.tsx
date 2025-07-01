7970fdffd998529d8d5ef1c25dcb24cbacba76fc
Seunghyum <antmgu@gmail.com>
2025-07-02 08:39:10 +0900

    feat: 홈 > 이미지 리스트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,41 @@
-export default async function Home() {
-  return <h1>헬로 월드</h1>;
+"use client";
+
+import Image from "next/image";
+import { useViewTypeStore } from "@/app/service-board/_containers/BoardView/useViewTypeStore";
+import { VIEW_TYPE } from "@/constants/viewType";
+import { useMemo } from "react";
+import { useIsMounted } from "./hooks/useIsMounted";
+
+const IMAGES = [
+  "img_homework_1.png",
+  "img_homework_2.png",
+  "img_homework_3.png",
+  "img_homework_4.png",
+];
+
+export default function Home() {
+  const viewType = useViewTypeStore((state) => state.viewType);
+  const isMounted = useIsMounted();
+  const images = useMemo(() => {
+    if (viewType === VIEW_TYPE.CARD) {
+      return [...IMAGES].reverse();
+    }
+    return IMAGES;
+  }, [viewType]);
+
+  return (
+    isMounted && (
+      <div className="grid grid-cols-2 gap-4 w-full">
+        {images.map((img) => (
+          <Image
+            key={img}
+            src={`/images/${img}`}
+            alt="image"
+            width={1000}
+            height={420}
+          />
+        ))}
+      </div>
+    )
+  );
 }
